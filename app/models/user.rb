448c5096739ca9f9ba47f8ca4998066aa88da569
Jair Hernandez <jairhdzgaytan@gmail.com>
2020-08-20 15:25:51 -0500

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   has_many :goats, dependent: :destroy
   has_many :rentals
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :photo, presence: true
 end
